e74144b947a0300cb51d4f0b33bd48e1646a756d
KIRUBAHARAN M <105787201+KIRUBAHARAN8878M@users.noreply.github.com>
2025-02-02 18:51:39 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { createOrder, getOrders } = require("../controllers/orderController");
+const { getUserOrders, placeOrder } = require("../controllers/orderController");
+const authMiddleware = require("../middleware/authMiddleware"); 
 
 const router = express.Router();
 
-router.post("/", createOrder);  // Create order
-router.get("/", getOrders);  // Fetch all orders
+
+router.get("/", authMiddleware, getUserOrders);
+
+router.post("/", authMiddleware, placeOrder);
 
 module.exports = router;
